04a47ddc7d58ed7ff7e1c9653097a7542fa96ddb
ifotn <Rich.Freeman@GeorgianCollege.ca>
2026-03-23 12:53:50 -0400

    l10/6 - login w/jwt cookie passed to client

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,31 @@
+export async function POST(req: Request) {
+    // get credentials from form request
+    const body = await req.json();
+
+    // try authenticating user in server api
+    const res: Response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/v1/users/login`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+        credentials: 'include'  // needed to receive cookie w/jwt from api
+    });
+
+    // evaluate response
+    if (!res.ok) {
+        const errorText = await res.text();
+        console.log(`Login error: ${errorText}`);
+        return new Response(errorText, { status: res.status });
+    }
+
+    // api call succeeds & returns 200, plus header containing our cookie w/jwt
+    // need to pass cookie to client so we have it for private api calls
+    const setCookieHeader = res.headers.get('set-cookie');
+    const responseHeaders = new Headers();
+
+    if (setCookieHeader) {
+        responseHeaders.set('Set-Cookie', setCookieHeader);
+    }
+
+    // return success response + http header that contains cookie w/jwt
+    return Response.json({ success: true }, { headers: responseHeaders });
+}
--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,15 +1,65 @@
+'use client';
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 export default function Login() {
+    const router = useRouter();
+
+    // state vars
+    const [username, setUsername] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
+    // state key/val dict of val errors
+    const [errors, setErrors] = useState<Record<string, string>>({});
+
+    const validate = (): boolean => {
+        const newErrors: Record<string, string> = {};
+
+        if (!username.trim()) newErrors.username = 'Username is required';
+        if (!password.trim()) newErrors.password = 'Password is required';
+
+        setErrors(newErrors);
+
+        if (Object.keys(newErrors).length === 0) return true;
+
+        return false;
+    }
+
+    const handleSubmit = async (e: React.SyntheticEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (!validate()) return;
+
+        // form valid => post user to server api
+        const res: Response = await fetch(`${process.env.NEXT_PUBLIC_CLIENT_URL}/api/auth/login`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username, password })
+        });
+
+        if (!res.ok) {
+            alert(res.text());
+            return;
+        }
+
+        // ok => redirect to games
+        router.push('/games');
+    }
+
     return (
         <main>
             <h1>Login</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <fieldset>
                     <label htmlFor="username">Username:</label>
-                    <input name="username" id="username" required />
+                    <input name="username" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                    {errors.username && <span className="error">{errors.username}</span>}
                 </fieldset>
                 <fieldset>
                     <label htmlFor="password">Password:</label>
-                    <input name="password" id="password" type="password" />
+                    <input name="password" id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    {errors.password && <span className="error">{errors.password}</span>}
                 </fieldset>
                 <button>Login</button>
             </form>
